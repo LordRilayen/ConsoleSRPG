8509e156d8aa0ccfc2c8a7220e4775e6cf6d1b63
Cody Owen <owenca93@gmail.com>
2020-02-06 15:11:16 -0500

    Added design notes and syncing new branch

--- game/main.cpp
+++ game/main.cpp
@@ -9,5 +9,11 @@ int main()
 	Entities::Survivor survivor;
 	survivor.GetCompleteStatus();
 
+	int test = (1 + 3) * 1.01;
+	float test2 = (1 + 3) * 1.01;
+
+	std::cout << test << std::endl;
+	std::cout << test2 << std::endl;
+
 	return 0;
 }

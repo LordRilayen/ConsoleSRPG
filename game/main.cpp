ae495778073e3ec2b492d59b1a95ee7539e73050
Cody Owen <owenca93@gmail.com>
2020-02-06 17:49:02 -0500

    One character can now attack another

--- game/main.cpp
+++ game/main.cpp
@@ -6,14 +6,24 @@
 
 int main()
 {
-	Entities::Survivor survivor;
-	survivor.GetCompleteStatus();
+	Entities::Survivor Roxas;
+	Entities::Survivor Vanitas;
+	Vanitas.SetName("Vanitas");
+	Vanitas.SetCriticalChance(50);
 
-	int test = (1 + 3) * 1.01;
+	Roxas.AttackTarget(Vanitas);
+
+	/*Roxas.GetCompleteStatus();
+	std::cout << " " << std::endl;
+	Vanitas.GetCompleteStatus();*/
+
+
+	/*int test = (1 + 3) * 1.01;
 	float test2 = (1 + 3) * 1.01;
 
+	std::cout << std::endl;
 	std::cout << test << std::endl;
-	std::cout << test2 << std::endl;
+	std::cout << test2 << std::endl;*/
 
 	return 0;
 }
